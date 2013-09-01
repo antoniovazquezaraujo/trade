3dba67e55def8c5546e6c3878b044a77b033a9b6
avaraujo <antoniovazquezaraujo@gmail.com>
2013-09-01 23:11:29 +0200

    Simplificado con traders, catalogo de objetos y listo para empezar con reacciones disparables

--- trade/Maqueta.h
+++ trade/Maqueta.h
@@ -1,23 +1,14 @@
 #include <iostream>
 #include <map>
 using namespace std;
-
-class Transferer;
-class Libro;
 class Producto {
 public:
 	Producto(string nombre = "generico") :
 			nombre(nombre), precio(0) {
 
 	}
-	virtual ~Producto() {
-
-	}
-	virtual void transferir(Transferer *c, Transferer* v,
-			unsigned int cantidad = 1);
 	string nombre;
 	float precio;
-
 };
 bool operator==(const Producto& a, const Producto& b) {
 	return a.nombre == b.nombre;
@@ -26,108 +17,89 @@ bool operator<(const Producto& a, const Producto& b) {
 	return a.nombre < b.nombre;
 }
 
-class Droga: public Producto {
+class Catalogo{
 public:
-	void transferir(Transferer *c, Transferer* v, unsigned int cantidad = 1);
+	static Producto* getProducto(int id){
+		if(productos.count(id)>0){
+			return productos[id];
+		}
+		return 0;
+	}
+	static void addProducto(Producto * p, int id){
+		productos[id]=p;
+	}
+	static map<int, Producto*> productos;
 };
+// productos del catalogo general
+map<int, Producto* > Catalogo::productos;
 
-class Libro: public Producto {
+Producto* getProducto(int id){
+	return Catalogo::getProducto(id);
+}
+
+class Trader {
 public:
-	void transferir(Transferer *c, Transferer* v, unsigned int cantidad = 1);
-
-};
-class Comida: public Producto {
-};
-
-class Transferer {
-public:
-	Transferer() :
+	Trader() :
 			saldo(0) {
 
 	}
-	virtual ~Transferer() {
+	virtual ~Trader() {
 
 	}
-	void agregar(Producto p, unsigned int cantidad) {
-		if (stock.count(p) > 0) {
-			stock[p] += cantidad;
+	void agregar(int productoId, unsigned int cantidad) {
+		if (stock.count(productoId) > 0) {
+			stock[productoId] += cantidad;
 		} else {
-			stock[p] = cantidad;
+			stock[productoId] = cantidad;
 		}
 	}
-	virtual bool entregar(Producto p, unsigned int cantidad) {
-		if (stock.count(p) >= cantidad) {
-			stock[p] -= cantidad;
-			saldo += cantidad * p.precio;
+	bool buy(int productoId, Trader * proveedor, int cantidad){
+		if(proveedor->send(productoId, cantidad)){
+			receive(productoId, cantidad);
+		}
+	}
+	bool sale(int productoId, Trader * cliente, int cantidad){
+		if(send(productoId, cantidad)){
+			cliente->receive(productoId, cantidad);
+		}
+	}
+	virtual bool send(int productoId, unsigned int cantidad) {
+		if (stock.count(productoId) >= cantidad) {
+			stock[productoId] -= cantidad;
+			saldo += cantidad *getProducto(productoId)->precio;
 			return true;
 		}
 		return false;
 	}
-	virtual bool recibir(Producto p, unsigned int cantidad) {
-		if (saldo >= cantidad * p.precio) {
-			stock[p] += cantidad;
-			saldo -= cantidad * p.precio;
+	virtual bool receive(int productoId, unsigned int cantidad) {
+		if (saldo >= cantidad * getProducto(productoId)->precio) {
+			stock[productoId] += cantidad;
+			saldo -= cantidad * getProducto(productoId)->precio;
 			return true;
 		}
 		return false;
 	}
-	virtual void entregarDroga(Droga d, int cantidad) {
-		entregar(d, cantidad);
-	}
-	virtual void entregarLibro(Libro l, int cantidad) {
-		entregar(l, cantidad);
-	}
-	virtual void recibirDroga(Droga d, int cantidad) {
-		recibir(d, cantidad);
-	}
-	virtual void recibirLibro(Libro l, int cantidad) {
-		recibir(l, cantidad);
-	}
-	int getStock(Producto p) {
-		if (stock.count(p) > 0) {
-			return stock[p];
+ 	int getStock(int productoId) {
+		if (stock.count(productoId) > 0) {
+			return stock[productoId];
 		}
 		return 0;
 	}
 	int getNumProductos() {
 		return stock.size();
 	}
-	map<Producto, int> stock;
+	map<int, int> stock;
 	float saldo;
 };
 
-void Producto::transferir(Transferer *c, Transferer* v, unsigned int cantidad) {
-	cout << "Me compran" << endl;
-}
-
-void Droga::transferir(Transferer *c, Transferer * v, unsigned int cantidad) {
-	c->entregarDroga(*this, cantidad);
-	v->recibirDroga(*this, cantidad);
-}
-void Libro::transferir(Transferer *c, Transferer * v, unsigned int cantidad) {
-	v->entregarLibro(*this, cantidad);
-	v->recibirLibro(*this, cantidad);
-}
-class Jugador: public Transferer {
+ class Jugador: public Trader {
 public:
 	virtual ~Jugador() {
 
 	}
-	void recibirDroga(Droga d, int cantidad) {
-		Transferer::recibirDroga(d, cantidad);
-		status = 234234;
-	}
+
 	int status;
 };
-class UsuarioAdicto: public Transferer{
-public:
-	void recibirDroga(Droga d, int cantidad) {
-		cout << "Muy buena esta droga a precio: " << d.precio << " es barata!" << endl;
-	}
 
-};
-void transferir(Producto p, Transferer *c, Transferer * v, unsigned int cantidad){
-	c->entregar(p, cantidad);
-	v->recibir(p, cantidad);
-}
+
 
--- trade/Maqueta_test.cpp
+++ trade/Maqueta_test.cpp
@@ -6,27 +6,29 @@
 using namespace std;
 BOOST_AUTO_TEST_CASE( maqueta_test )
 {
-	Droga prod;
-	prod.precio = 10;
-	Jugador * j1 = new Jugador;
-	Jugador * j2 = new Jugador;
-	j1->saldo = 10;
-	j2->saldo = 10;
-	BOOST_CHECK(j1->getStock(prod) == 0);
-	BOOST_CHECK(j2->getStock(prod) == 0);
-	j1->agregar(prod, 10);
-	BOOST_CHECK(j1->getStock(prod) == 10);
-	BOOST_CHECK(j2->getStock(prod) == 0);
-	prod.transferir(j1, j2, 1);
-//	cout  << j1->saldo << ", " << j2->saldo<<endl;
-	BOOST_CHECK(j1->saldo==20);
-	BOOST_CHECK(j2->saldo==0);
-	BOOST_CHECK(j1->getStock(prod) == 9);
-	BOOST_CHECK(j2->getStock(prod) == 1);
+	Producto p("naranja");
+	p.precio = 10;
+	Producto q("manzana");
+	q.precio = 20;
+	Catalogo::addProducto(&p, 1);
+	Catalogo::addProducto(&q, 2);
+
+	Trader * j1 = new Trader;
+	Trader * j2 = new Trader;
+	j1->saldo = 30;
+	j2->saldo = 30;
+	BOOST_CHECK(j1->getStock(1) == 0);
+	BOOST_CHECK(j2->getStock(2) == 0);
+	j1->agregar(1, 10);
+	BOOST_CHECK(j1->getStock(1) == 10);
+	BOOST_CHECK(j2->getStock(2) == 0);
+	j1->sale(1, j2, 1);
+	BOOST_CHECK(j1->saldo==40);
+	BOOST_CHECK(j2->saldo==20);
+	BOOST_CHECK(j1->getStock(1) == 9);
+	BOOST_CHECK(j2->getStock(1) == 1);
 	BOOST_CHECK(j1->getNumProductos()==1);
-	UsuarioAdicto * u = new UsuarioAdicto;
-	prod.transferir(j2, u, 1);
-	prod.transferir(u, j2, 1);
+
 
 
 
